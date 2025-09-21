aab9105be1e440b9109bcd2b0f597f83b2d81f4d
Mohammad Jouza <m.jozaa@yahoo.com>
2025-09-21 16:08:51 +0300

    w02d01

---
+++ LC-Week-02/LC-W02D01/1-intro.py
@@ -0,0 +1,99 @@
+
+# 1
+# 2
+
+""" 
+bla
+bla
+bla
+b
+
+ """
+
+
+print('Hello World') #comment
+
+print('25-09-21')
+
+num1 = 5
+hey_there = 'c2'
+# hey_there = 'sandwich'
+hey_There = 'bla bla'
+# const / let name = value
+   
+# JS: camelCase => heyThere VS HeyThere
+# PY: snake_case => hey_there VS hey_There
+
+# Extra - HW: JS what will happened if we defined TWO variables 'heyThere', what will happen din PY (how we can fix that/ protect our self)
+
+""" 
+YES: 10-50 hr
+NO: 0 hr in py
+<<: 0-10 h py
+>>: 50-100 py
+
+ """
+
+num_1=55
+# Reassigning  
+# JS in varName= newValue
+print("40-first time:",num_1)
+num_1= 33
+print("42-second time:",num_1)
+
+# 1  1.0 2.5
+
+
+# let beverage = age >= 21 ? 'Tea
+# 
+# ' : 'Milk';
+#     beverage = 'Tea
+# 
+# ' if age >= 21 else 'Milk'
+
+
+
+# !- HW read about
+#     PY: `list` / `tuple` / `dictionary`
+#     JS: Array VS Object
+
+# Extra Extra HW - JS how to achieve this
+mult_str=""" 
+bla bla
+test
+hello
+ """
+
+
+# E.3- Extra Extra HW - JS how to achieve this
+# Py: F-strings (Formatted String Literals)
+
+num_of_stu = 31
+class_name="SE Cohort-2"
+
+# We have `31` students in class `SE Cohort-2`
+result_1= f"We have: {num_of_stu} students in class: {class_name} "
+print("RESULT:",result_1 )
+
+
+# HW- Try this in PY VS JS
+# num = 25
+# msg = "There are " + num + " tacos"
+
+""" 
+# JS
+const num = 25
+let msg = "There are " + num + " tacos"
+ """
+
+
+
+
+
+""" 
+YS:
+NO:
+<<: 
+>>: 
+
+ """
---
+++ LC-Week-02/LC-W02D01/2-control.py
@@ -0,0 +1,127 @@
+
+
+
+
+
+
+# YS: if
+# NO: didn't work with if
+
+# for / while / switch
+# YS: if
+# NO: didn't work with if
+
+
+
+# !- HW 1- Truthy & Falsey in Python
+# normal value : 5, 25, 1.4 , -8
+
+# !- HW 2- with and / or
+# 5 and 2,  -1 or 0, 1 or 0 or 5
+
+
+# 5 > 2 True
+# 2 < -1 False
+
+# %
+
+# !- HW 3-  25 > 1.5,  5 > "44"
+# try these
+
+#  you don't have a bicycle
+# NO => YEs
+# YES => No
+
+# you have a bicycle
+# No
+# Yes
+
+
+# TODO: Control Flow
+
+""" 
+in JS
+sum(){
+  line 1
+  line 2
+  line 3
+}
+line 4
+
+
+PY
+sum  / sum()   / sum:
+  line 1
+  line 2
+  line 3
+
+line 4
+ """
+
+floor = "sticky"
+walls = "clean"
+if floor == "sticky":   # don't forget the colon
+  print("1- Clean the floor! It's sticky!")
+  # more lines of code in this code
+  # block need to be indented as well
+if walls == "sticky":
+  print("2- Clean the walls! They're sticky!")
+
+""" 
+what we will got (print)
+YS: both of them 1 + 2
+NO: Nothing
+<<: ONLY 1
+>>: ONLY 2
+CF: others
+
+ """
+
+
+
+
+
+
+
+
+# !- IMP: 1.1- 💪 Branching Exercise (10 minutes)
+# https://git.generalassemb.ly/Jordan-Modee/python-control-flow-1?tab=readme-ov-file#-branching-exercise-10-minutes
+
+# !- IMP: 1.2- 💪 Looping Exercise (10 minutes)
+# https://git.generalassemb.ly/Jordan-Modee/python-control-flow-1?tab=readme-ov-file#-looping-exercise-10-minutes
+
+# !- IMP: 2- Lab
+# Extra Extra HW- JS: read about for in and for of
+
+names = ["Tom", "Deborah", "Murray", "Axel"]
+
+for name in names:
+  print("name:",name)
+
+for stu in names:
+  print("stu:",stu)
+
+index_1= 0
+
+foods = ["Shawerma", "Falafel", "Mansaf"]
+
+while index_1 < 3:
+  print("food:",foods[index_1])
+
+  index_1 +=1
+
+
+print(index_1)
+# Blue Green Yellow Orange RED
+# !- E.5- JS: what will happened
+"""
+YS: it will work => Infinite
+NO: Error (array have only 3 element)
+>>: noting will print
+<<: others
+    """
+
+# !- 3- PY: read about Break
+# !- 4- PY: read about Ranges + Negative Ranges
+# !- 5- PY: play with Ranges + Negative Ranges
+
