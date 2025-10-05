bbc1f98cc8e7c38f2647191088258a2de513f6ef
Mohammad Jouza <m.jozaa@yahoo.com>
2025-10-05 16:00:36 +0300

    end of W04D01

--- LC-Week-04/LC-W04D01/django-crud-app-cat-collector/main_app/models.py
+++ LC-Week-04/LC-W04D01/django-crud-app-cat-collector/main_app/models.py
@@ -15,3 +15,32 @@ class Cat(models.Model):
 # cat_1 = Cat()
 # record  Table
 # c = Cat(name='Biscuit', breed='Sphinx', description='Cuddle monster. Hairless.', age=2)
+
+
+""" 
+try:
+    id_s= 2
+    cat = Cat.objects.get(id=id_s)
+except Cat.DoesNotExist:
+    # Handle the case where the object does not exist
+    print(f"This cat: {id_s} - does not exist!")
+
+try:
+    id_s= 1
+    cat = Cat.objects.get(id=id_s)
+    print(f"This cat: {id_s} - IS EXIST!")
+except Cat.DoesNotExist:
+    # Handle the case where the object does not exist
+    print(f"This cat: {id_s} - does not exist!")
+
+
+
+Cat.objects.filter(age=2)
+
+
+Cat.objects.filter(age=0)
+Cat.objects.filter(age__gte=1)
+
+Cat.objects.order_by('-name')
+
+ """
