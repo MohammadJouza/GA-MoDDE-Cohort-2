5ca8e8466d922187aeabb9a912e216ed6268037b
Mohammad Jouza <m.jozaa@yahoo.com>
2025-10-05 14:02:53 +0300

    W04D01 - Live Coding - lect 2

--- LC-Week-04/LC-W04D01/django-crud-app-cat-collector/main_app/models.py
+++ LC-Week-04/LC-W04D01/django-crud-app-cat-collector/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# models = tables
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+# cat_1 = Cat()
+# record  Table
+# c = Cat(name='Biscuit', breed='Sphinx', description='Cuddle monster. Hairless.', age=2)
