cc7330c85977d78463b092232fbaccffce1992e1
Mohammad Jouza <m.jozaa@yahoo.com>
2025-10-06 12:36:59 +0300

    W04D01 - first lecture

--- LC-Week-04/LC-W04D02/django-crud-app-cat-collector/main_app/urls.py
+++ LC-Week-04/LC-W04D02/django-crud-app-cat-collector/main_app/urls.py
@@ -1,27 +1,28 @@
 # main_app/urls.py
 
 from django.urls import path
-from . import views 
+from . import views
+
 # Import views to connect routes to view functions
 
 urlpatterns = [
+    # !- HW Extra SSOT - Single source of truth
     # Routes will be added here
-    path('', views.home, name='home'),
-    path('cats/', views.cat_index, name='cat-index'),
-    path('about/', views.about, name='home'),
+    path("", views.home, name="home"),
+    path("cats/", views.cat_index, name="cat-index"),
+    path("about/", views.about, name="about"),
     # endpoint  cats/1 => cat_id:1    cats/2 => cat_id:2
-    path('cats/<int:cat_id>/', views.cat_detail, name='cat-index'),
+    path("cat/<int:cat_id>/", views.cat_detail, name="cat-detail"),
     # path('/careers', views.career, name='home'),
     # !- HW 3.b Handle id for other endpoint dont have /blabla
 ]
- 
+
 
 # STEPS:
-# // !- 0- Problem variable inside the path 
+# // !- 0- Problem variable inside the path
 # 1- ULR: 'cats/5'
 # 2- Interface (user interact with)
 #  // !- DONE (what about cat.id)
 # 3- Define the route
 # 4- Create the view
 # 5- Handle the response
-
