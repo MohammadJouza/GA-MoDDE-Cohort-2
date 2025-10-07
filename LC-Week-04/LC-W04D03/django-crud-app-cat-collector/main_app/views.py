0abe1aa816a31b5bd8ccd22d117fece9211ddd30
Mohammad Jouza <m.jozaa@yahoo.com>
2025-10-07 23:54:28 +0300

    solve remove

--- LC-Week-04/LC-W04D03/django-crud-app-cat-collector/main_app/views.py
+++ LC-Week-04/LC-W04D03/django-crud-app-cat-collector/main_app/views.py
@@ -1,13 +1,14 @@
 # main_app/views.py
 
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView 
-# add these 
+from django.views.generic import ListView, DetailView
+
+# add these
 
 from django.http import HttpResponse
-from .models import Cat,Toy
+from .models import Cat, Toy
 
 from .forms import FeedingForm
 
@@ -33,9 +34,12 @@ class CatDelete(DeleteView):
     model = Cat
     success_url = "/cats/"
 
+
 class ToyCreate(CreateView):
     model = Toy
-    fields = '__all__'
+    fields = "__all__"
+
+
 # Define the home view function
 
 
@@ -44,16 +48,19 @@ class ToyList(ListView):
     # {name: mouse, color: 'green'}
     # border-color:green
 
+
 class ToyDetail(DetailView):
     model = Toy
 
+
 class ToyUpdate(UpdateView):
     model = Toy
-    fields = ['name', 'color']
+    fields = ["name", "color"]
+
 
 class ToyDelete(DeleteView):
     model = Toy
-    success_url = '/toys/'
+    success_url = "/toys/"
 
 
 def home(request):
@@ -73,15 +80,18 @@ def cat_detail(request, cat_id):
     # !- how can I get information from the urls
     cat = Cat.objects.get(id=cat_id)
     # !- HW 3.A Handle id for other cats dont have
-    # toys = Toy.objects.all()  
-    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+    # toys = Toy.objects.all()
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list("id"))
 
     # Fetch all toys
     # print("CAT_ID: ", cat_id)
     feeding_form = FeedingForm()
     return render(
-        request, "cats/detail.html", {"cat": cat, "feeding_form": feeding_form, 'toys': toys_cat_doesnt_have}
+        request,
+        "cats/detail.html",
+        {"cat": cat, "feeding_form": feeding_form, "toys": toys_cat_doesnt_have},
     )
+
 
 def add_feeding(request, cat_id):
     # create a ModelForm instance using the data in request.POST
@@ -94,17 +104,30 @@ def add_feeding(request, cat_id):
         new_feeding = form.save(commit=False)
         new_feeding.cat_id = cat_id
         new_feeding.save()
-    return redirect('cat-detail', cat_id=cat_id)
+    return redirect("cat-detail", cat_id=cat_id)
+
 
 def associate_toy(request, cat_id, toy_id):
     # Note that you can pass a toy's id instead of the whole object
     # Model.
     Cat.objects.get(id=cat_id).toys.add(toy_id)
-    return redirect('cat-detail', cat_id=cat_id)
+    return redirect("cat-detail", cat_id=cat_id)
+
+
 # !- HW 1- read about run the server without install all the packages
 # !- HW 2- read about RESTful API
 
 #  !- HW Read About csrf_token cross request / cross origin
+
+
+def remove_toy(request, cat_id, toy_id):
+    # Look up the cat
+    cat = Cat.objects.get(id=cat_id)
+    # Look up the toy
+    toy = Toy.objects.get(id=toy_id)
+    # Remove the toy from the cat
+    cat.toys.remove(toy)
+    return redirect("cat-detail", cat_id=cat.id)
 
 
 """ 
